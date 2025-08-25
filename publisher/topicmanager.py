a00d9ef8965c306edad3f8b1425ecc74798faf06
Eclipse <Eclips77@users.noreply.github.com>
2025-08-25 16:43:28 +0300

    Refactor NewsGroupSampler to encapsulate category definitions and data fetching

--- publisher/topicmanager.py
+++ publisher/topicmanager.py
@@ -1,29 +1,43 @@
 from sklearn.datasets import fetch_20newsgroups
-# import logging
+class NewsGroupSampler:
+    def __init__(self):
+        self.interesting_categories = [
+            'alt.atheism',
+            'comp.graphics',
+            'comp.os.ms-windows.misc',
+            'comp.sys.ibm.pc.hardware',
+            'comp.sys.mac.hardware',
+            'comp.windows.x',
+            'misc.forsale',
+            'rec.autos',
+            'rec.motorcycles',
+            'rec.sport.baseball'
+        ]
+        self.not_interesting_categories = [
+            'rec.sport.hockey',
+            'sci.crypt',
+            'sci.electronics',
+            'sci.med',
+            'sci.space',
+            'soc.religion.christian',
+            'talk.politics.guns',
+            'talk.politics.mideast',
+            'talk.politics.misc',
+            'talk.religion.misc'
+        ]
+        self.interesting = fetch_20newsgroups(subset='all', categories=self.interesting_categories)
+        self.not_interesting = fetch_20newsgroups(subset='all', categories=self.not_interesting_categories)
 
-interesting_categories = [
-	'alt.atheism',
-	'comp.graphics',
-	'comp.os.ms-windows.misc',
-	'comp.sys.ibm.pc.hardware',
-	'comp.sys.mac.hardware',
-	'comp.windows.x',
-	'misc.forsale',
-	'rec.autos',
-	'rec.motorcycles',
-	'rec.sport.baseball'
-]
-not_interesting_categories = [
-	'rec.sport.hockey',
-	'sci.crypt',
-	'sci.electronics',
-	'sci.med',
-	'sci.space',
-	'soc.religion.christian',
-	'talk.politics.guns',
-	'talk.politics.mideast',
-	'talk.politics.misc',
-	'talk.religion.misc'
-]
-newsgroups_interesting = fetch_20newsgroups(subset='all', categories=interesting_categories)
-newsgroups_not_interesting = fetch_20newsgroups(subset='all', categories=not_interesting_categories)
+    def get_sample(self, total=20)-> dict:
+        half = total // 2
+        interesting_msgs = self.interesting.data[:half]
+        not_interesting_msgs = self.not_interesting.data[:half]
+        return {
+            "interesting": interesting_msgs,
+            "not_interesting": not_interesting_msgs
+        }
+
+# if __name__ == "__main__":
+    # x = NewsGroupSampler()
+    # z = x.get_sample()
+    # print(z)
